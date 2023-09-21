26b0dda4e9486a75c392b850378b5cc2643dd91e
hiraeth1024 <2415779164@qq.com>
2023-09-21 16:02:47 +0800

    init

---
+++ src/views/yygh/hospset/list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container">
+    <el-table :data="rows" stripe style="width: 100%">
+      <el-table-column prop="id" label="id" width="180"> 
+      </el-table-column>
+      <el-table-column prop="createTime" label="创建时间" width="180"> 
+      </el-table-column>
+      <el-table-column prop="updateTime" label="更新时间"> 
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+import hospset from "@/api/hospset";
+
+export default {
+  data() {
+    return {
+      //变量：值
+      rows: [],
+      total: 0,
+      searchObj:{}
+    };
+  },
+  methods: {},
+  created() {
+    hospset.getHospsetPage(1, 5,this.searchObj).then((response) => {
+      this.total = response.data.total;
+      this.rows = response.data.rows;
+    });
+  },
+};
+</script>
---
+++ src/views/yygh/hospset/save.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="app-container">
+        医院设置添加
+    </div>
+</template>
+
+<script>
+
+
+</script>
